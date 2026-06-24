04a456456b62c6e024f9940da55a003c6e72a15d
bashirSakka <bashirsakka003@gmail.com>
2026-06-24 19:46:36 +0300

    Add Railway and Vercel origins to CORS config

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,11 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://gitapirequests-production.up.railway.app',
+    ],
+    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
