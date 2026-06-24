0dc238326f58c7eb598201e31527897293ad9421
bashirSakka <bashirsakka003@gmail.com>
2026-06-24 19:53:51 +0300

    Add Vercel production URL to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://gitapirequests-production.up.railway.app',
+        'https://student-productivety-system-21s4mtsea-bashirsakkas-projects.vercel.app',
     ],
     'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
